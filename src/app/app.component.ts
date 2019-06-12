c8ad51ccd746b955e922dc3d6ff7e79ea88526ab
Tester Tester <bokangmogaladi@gmail.com>
2019-06-12 08:58:47 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '2S Technology Solutions';
+  title = 'Sign In';
+  weekdays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+  isavailable=true;
+
+  myClickFuntion(event)
+  {
+      alert("Button is clicked");
+      console.log(event);
+  }
+
+  weekdayChange(event)
+  {
+    alert("Weekeday changed");
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
